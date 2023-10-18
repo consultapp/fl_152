be914575473c23ad4ecfd04aa887d7c289675c81
Dmitry Egorov <info@consultapp.ru>
2023-10-18 14:23:17 +0300

    fix + routes

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import withRouter from '../hooks/withRouter.jsx';
-import { Home } from '../pages/home/index.jsx';
-import { Portfolio } from '../pages/portfolio/index.jsx';
-import { ContactUs } from '../pages/contact/index.jsx';
-import { About } from '../pages/about/index.jsx';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import withRouter from "../hooks/withRouter.jsx";
+import { Home } from "../pages/home/index.jsx";
+import { Portfolio } from "../pages/portfolio/index.jsx";
+import { ContactUs } from "../pages/contact/index.jsx";
+import { About } from "../pages/about/index.jsx";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const AnimatedRoutes = withRouter(({ location }) => (
   <TransitionGroup>
@@ -15,15 +15,12 @@ const AnimatedRoutes = withRouter(({ location }) => (
         enter: 400,
         exit: 400,
       }}
-      classNames='page'
+      classNames="page"
       unmountOnExit
     >
       <Routes location={location}>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/portfolio' element={<Portfolio />} />
-        <Route path='/contact' element={<ContactUs />} />
-        <Route path='*' element={<Home />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </CSSTransition>
   </TransitionGroup>
@@ -31,7 +28,7 @@ const AnimatedRoutes = withRouter(({ location }) => (
 
 function AppRoutes() {
   return (
-    <div className='s_c'>
+    <div className="s_c">
       <AnimatedRoutes />
     </div>
   );
